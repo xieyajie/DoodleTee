5f7e4f92ad4f88ba36d4e6d16d5c34f1ee0337c1
dhcdht <dhcdht@qq.com>
2013-07-06 08:19:44 +0800

    temp

--- DoodleTee/Classes/Effect/EffectPicker/XDDrawPicker.m
+++ DoodleTee/Classes/Effect/EffectPicker/XDDrawPicker.m
@@ -9,6 +9,10 @@
 #import "XDDrawPicker.h"
 
 @interface XDDrawPicker ()
+{
+    CGPoint _lastDrowCyclePoint;
+    CGFloat _lastDrowCycleRadius;
+}
 
 @end
 
@@ -27,5 +31,72 @@
     return self;
 }
 
+- (void)randomACycleAtPoint:(CGPoint)point
+{
+    [self drawCycleWithPoint: point
+                      radius: random()%10
+                       color: [UIColor colorWithRed: random()%255/255.0f
+                                              green: random()%255/255.0f
+                                               blue: random()%255/255.0f
+                                              alpha: random()%255/255.0f]];
+}
+
+- (void)drawCycleWithPoint:(CGPoint)point radius:(CGFloat)radius color:(UIColor*)color
+{
+    CGRect cycleRect = CGRectMake(0, 0, 2*radius, 2*radius);
+    
+    UIGraphicsBeginImageContextWithOptions(cycleRect.size, NO, [[UIScreen mainScreen] scale]);
+    CGContextRef context = UIGraphicsGetCurrentContext();
+    
+    CGContextSetFillColorWithColor(context, color.CGColor);
+    CGContextFillEllipseInRect(context, cycleRect);
+    
+    UIImage *image = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    
+    UIImageView *imageView = [[UIImageView alloc] initWithImage: image];
+    imageView.center = point;
+    [self.effectView addSubview: imageView];
+    [imageView release];
+    
+    _lastDrowCyclePoint = point;
+    _lastDrowCycleRadius = radius;
+}
+
+- (CGFloat)distanceForPoint:(CGPoint)point andPoint:(CGPoint)anotherPoint
+{
+    CGFloat distance = sqrt((point.x*point.x - anotherPoint.x*anotherPoint.x) + (point.y*point.y - anotherPoint.y*anotherPoint.y));
+    
+    return distance;
+}
+
+#pragma mark - Override Touch Methods
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView: self.effectView];
+    
+    [self randomACycleAtPoint: point];
+}
+
+- (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView: self.effectView];
+    
+    if ([self distanceForPoint: _lastDrowCyclePoint andPoint: point] >= 2*_lastDrowCycleRadius)
+    {
+        [self randomACycleAtPoint: point];
+    }
+}
+
+- (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    UITouch *touch = [touches anyObject];
+    CGPoint point = [touch locationInView: self.effectView];
+    
+    [self randomACycleAtPoint: point];
+}
 
 @end
--- DoodleTee/Classes/Effect/XDEffectViewController.m
+++ DoodleTee/Classes/Effect/XDEffectViewController.m
@@ -760,5 +760,4 @@ typedef enum{
     }
 }
 
-
 @end
